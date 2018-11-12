b8b221123251882e176c95c817ceb046122d2368
Azeem Hassni <azeem.hassni@thebrandcrew.com>
2018-11-12 18:47:15 +0500

    Added models and migrations

---
+++ database/migrations/2018_11_12_133448_create_questionnaires_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionnairesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questionnaires', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('duration');
+            $table->enum('time_in', ['Minutes', 'Hours']);
+            $table->boolean('can_resume')->default(false);
+            $table->boolean('is_published')->default(true);
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questionnaires');
+    }
+}
