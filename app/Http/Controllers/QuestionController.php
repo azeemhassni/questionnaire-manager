b0df3f35b91a05a6a726d15c6fd4355baef554e3
Azeem Hassni <azeem.hassni@thebrandcrew.com>
2018-11-13 22:12:24 +0500

    Controller cleanup

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -8,15 +8,6 @@ use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
 
     /**
      * Show the form for creating a new resource.
@@ -54,51 +45,5 @@ class QuestionController extends Controller
         }
 
         return redirect()->route('questionnaire.index')->with(['message' => 'Questions has been added successfully']);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Question $question
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Question $question)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param Questionnaire $questionnaire
-     * @param  \App\Question $question
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Questionnaire $questionnaire, Question $question)
-    {
-        return view('question.edit', compact('questionnaire', 'question'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  \App\Question $question
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Question $question)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Question $question
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Question $question)
-    {
-        //
     }
 }
